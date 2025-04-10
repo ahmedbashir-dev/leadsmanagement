6a84087d4a6a1a4426f1afc4379888b684bbdb1c
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-04-11 04:47:02 +0500

    Use frontend url from properties

--- src/main/java/com/leads/leadmanagement/config/CorsConfig.java
+++ src/main/java/com/leads/leadmanagement/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.leads.leadmanagement.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,11 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${frontend.url}")
+    private String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/leads")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
