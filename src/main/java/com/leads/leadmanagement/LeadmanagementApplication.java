3c4dbb12b3f0e19f0bf1a61b075f9ad62e41ddb2
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-04-11 04:20:35 +0500

    Cors Configuration and prod properties added

--- src/main/java/com/leads/leadmanagement/LeadmanagementApplication.java
+++ src/main/java/com/leads/leadmanagement/LeadmanagementApplication.java
@@ -2,8 +2,10 @@ package com.leads.leadmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.leads.leadmanagement")
 public class LeadmanagementApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/leads/leadmanagement/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.leads.leadmanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/leads")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
