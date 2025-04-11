5596e0d71bd2725b5e37349675227bd1d23653e3
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-04-11 14:48:25 +0500

    Env Logs

--- src/main/java/com/leads/leadmanagement/LeadmanagementApplication.java
+++ src/main/java/com/leads/leadmanagement/LeadmanagementApplication.java
@@ -1,15 +1,28 @@
 package com.leads.leadmanagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.leads.leadmanagement")
-public class LeadmanagementApplication {
+public class LeadmanagementApplication implements CommandLineRunner {
+
+	@Autowired
+	private Environment env;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeadmanagementApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) {
+		System.out.println("Database URL: " + env.getProperty("SPRING_DATASOURCE_URL"));
+		System.out.println("Database Username: " + env.getProperty("SPRING_DATASOURCE_USERNAME"));
+	}
 }
